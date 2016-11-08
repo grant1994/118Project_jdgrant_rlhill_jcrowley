75309276b4ff62e1e7e477ab7986d0d196bc195a
grant1994 <grant1994@ymail.com>
2016-11-07 17:25:50 -0800

    Initial upload of our project directory
    This is our initial project! :) Currently we only have the initial SyncSampling service here for our tape sensors.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=118Project_jdgrant_rlhill_jcrowley.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/118Project_jdgrant_rlhill_jcrowley.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=118projectjdgrantrlhilljcrowley.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/118projectjdgrantrlhilljcrowley.tar
