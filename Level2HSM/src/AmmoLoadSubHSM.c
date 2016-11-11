166a11997ef4a75bfadb88393bf17efd1180ca0d
Joseph Grant <Joseph Grant>
2016-11-10 18:27:00 -0800

    Tryna add AmmoLoadSubHSM yo.

--- Level2HSM/src/AmmoLoadSubHSM.c
+++ Level2HSM/src/AmmoLoadSubHSM.c
@@ -1,5 +1,5 @@
 /*
- * File:   AmmoSearchSubHSM.h
+ * File:   AmmoLoadSubHSM.h
  * Author: jdgrant
  *
  * This file includes the top level of our hierarchal state machine. At this level
@@ -15,7 +15,7 @@
 #include "ES_Configure.h"
 #include "ES_Framework.h"
 #include "BOARD.h"
-#include "AmmoSearchSubHSM.h"
+#include "AmmoLoadSubHSM.h"
 #include "sensors.h"
 #include "motor.h"
 
@@ -27,17 +27,14 @@
  * MODULE #DEFINES                                                             *
  ******************************************************************************/
 
-
 typedef enum {
     InitPState,
-    Forward,
     TankTurn,
     Backward,
 } HSMState_t;
 
 static const char *StateNames[] = {
 	"InitPState",
-	"Forward",
 	"TankTurn",
 	"Backward",
 };
@@ -64,14 +61,14 @@ static uint8_t MyPriority;
  ******************************************************************************/
 
 /**
- * @Function InitAmmoSearchSubHSM(uint8_t Priority)
+ * @Function InitAmmoLoadSubHSM(uint8_t Priority)
  * @param Priority - internal variable to track which event queue to use
  * @return TRUE or FALSE
  * @brief This will get called by the framework at the beginning of the code
  *        execution. It will post an ES_INIT event to the appropriate event
- *        queue, which will be handled inside RunAmmoSearchSubHSM function.
+ *        queue, which will be handled inside RunAmmoLoadSubHSM function.
  */
-uint8_t InitAmmoSearchSubHSM(uint8_t Priority)
+uint8_t InitAmmoLoadSubHSM(uint8_t Priority)
 {
     MyPriority = Priority;
     // put us into the Initial PseudoState
@@ -85,25 +82,25 @@ uint8_t InitAmmoSearchSubHSM(uint8_t Priority)
 }
 
 /**
- * @Function PostAmmoSearchSubHSM(ES_Event ThisEvent)
+ * @Function PostAmmoLoadSubHSM(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be posted to queue
  * @return TRUE or FALSE
  * @brief This function is a wrapper to the queue posting function.
  */
-uint8_t PostAmmoSearchSubHSM(ES_Event ThisEvent)
+uint8_t PostAmmoLoadSubHSM(ES_Event ThisEvent)
 {
     return ES_PostToService(MyPriority, ThisEvent);
 }
 
 /**
- * @Function RunAmmoSearchSubHSM(ES_Event ThisEvent)
+ * @Function RunAmmoLoadSubHSM(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be responded.
  * @return Event - return event (type and param), in general should be ES_NO_EVENT
  * @brief This is the implementation for the top level of our state machine and 
  *        it contains the basic logic for our autonomous robot to meet the minimum
  *        specs of the competition.
  */
-ES_Event RunAmmoSearchSubHSM(ES_Event ThisEvent)
+ES_Event RunAmmoLoadSubHSM(ES_Event ThisEvent)
 {
     uint8_t makeTransition = FALSE; // use to flag transition
     static uint8_t turnParam; // use this flag to turnCW or turnCCW
@@ -119,77 +116,27 @@ ES_Event RunAmmoSearchSubHSM(ES_Event ThisEvent)
             // transition from the initial pseudo-state into the actual
             // initial state
             // Initialize all sub-state machines
-            //InitAmmoSearchHSM();
+            //InitAmmoLoadHSM();
             // now put the machine into the actual initial state
-            nextState = Forward;
+            nextState = TankTurn;
             makeTransition = TRUE;
             ThisEvent.EventType = ES_NO_EVENT;
             ;
         }
         break;
 
-    case Forward: // in the first state, replace this with correct names
-        // run sub-state machine for this state
-        //NOTE: the SubState Machine runs and responds to events before anything in the this
-        //state machine does
-        //ThisEvent = RunAmmoSearchHSM(ThisEvent);
-        switch (ThisEvent.EventType) {
-            case TS_FL_TRIGGERED:
-                if (thisEvent.EventParam)
-                {
-                    turnParam = 1;
-                    nextState = Backward;
-                    makeTransition = TRUE;
-                }
-                break;
-            case TS_FR_TRIGGERED:
-                if (thisEvent.EventParam)
-                {
-                    turnParam = 0;
-                    nextState = Backward;
-                    makeTransition = TRUE;
-                }
-                break;
-            case BUMPED:
-                // We kinda want to ignore back bumpers when going forward, who cares if a robot hit us
-                if (thisEvent.EventParam == 0x1)
-                {
-                    turnParam = 0;
-                    nextState = Backward;
-                    makeTransition = TRUE;
-                } 
-                else if (thisEvent.EventParam == 0x2)
-                {
-                    turnParam = 1;
-                    nextState = Backward;
-                    makeTransition = TRUE;
-                }
-                break;
-            case ES_ENTRY:
-                moveForward();
-                break;
-            case ES_NO_EVENT:
-            default:
-                break;
-        }
-        break;
-        
     case TankTurn:
         switch (ThisEvent.EventType) {  
             case ES_ENTRY:
-                ES_Timer_InitTimer(SHORT_HSM_TIMER, SHORT_TIMER_TICKS);
-                if (turnParam)
-                {
-                    tankTurnRight();
-                }
-                else
-                {
-                    tankTurnLeft();
-                }
+                tankTurnRight();
                 break;
-            case ES_TIMEOUT:
-                nextState = Forward;
-                makeTransition = TRUE;
+            case TW_TRIGGERED:
+                // This value assumes we are using only middle and back track wires, meaning both bits are set high
+                if (thisEvent.EventParam == 0x3)
+                {
+                    nextState = Backward;
+                    makeTransition = TRUE;
+                }
                 break;
             case ES_NO_EVENT:
             default:
@@ -200,12 +147,15 @@ ES_Event RunAmmoSearchSubHSM(ES_Event ThisEvent)
     case Backward:
         switch (ThisEvent.EventType) {
             case ES_ENTRY:
-                ES_Timer_InitTimer(SHORT_HSM_TIMER, SHORT_TIMER_TICKS);
                 moveBackward();
                 break;
-            case ES_TIMEOUT:
-                nextState = TankTurn;
-                makeTransition = TRUE;
+            case TW_TRIGGERED:
+                // This value assumes we are using only middle and back track wires, meaning both bits are set high
+                if (thisEvent.EventParam != 0x3)
+                {
+                    nextState = TankTurn;
+                    makeTransition = TRUE;
+                }
                 break;
             case ES_NO_EVENT:
             default:
@@ -219,9 +169,9 @@ ES_Event RunAmmoSearchSubHSM(ES_Event ThisEvent)
 
     if (makeTransition == TRUE) { // making a state transition, send EXIT and ENTRY
         // recursively call the current state with an exit event
-        RunAmmoSearchSubHSM(EXIT_EVENT); // <- rename to your own Run function
+        RunAmmoLoadSubHSM(EXIT_EVENT); // <- rename to your own Run function
         CurrentState = nextState;
-        RunAmmoSearchSubHSM(ENTRY_EVENT); // <- rename to your own Run function
+        RunAmmoLoadSubHSM(ENTRY_EVENT); // <- rename to your own Run function
     }
 
     ES_Tail(); // trace call stack end
